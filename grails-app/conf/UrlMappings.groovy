6712e03ed4aab7005a35f52697a0f97b60e692b4
Orlando Becerra <orlandobcrra@gmail.com>
2012-02-20 16:21:42 -0430

    cname1

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,11 @@
 class UrlMappings {
 
-	static mappings = {
+    static mappings = {
 		
-		/* 
-		 * Pages without controller 
-		 */
-//		"/"				(view:"/index")
+        /* 
+         * Pages without controller 
+         */
+        //		"/"				(view:"/index")
 		"/about"		(view:"/siteinfo/about")
 		"/blog"			(view:"/siteinfo/blog")
 		"/systeminfo"	(view:"/siteinfo/systeminfo")
@@ -13,24 +13,31 @@ class UrlMappings {
 		"/terms"		(view:"/siteinfo/terms")
 		"/imprint"		(view:"/siteinfo/imprint")
 		
-		/* 
-		 * Pages with controller
-		 * WARN: No controller should be named "api" or "mobile" or "web"!
-		 */
+        /* 
+         * Pages with controller
+         * WARN: No controller should be named "api" or "mobile" or "web"!
+         */
+        
+        "/admin/cfDashboard/$action?"(controller: 'cloudFoundryDashboard')
+        "/admin/cfDashboard/application/$appName"(controller: 'cloudFoundryDashboard', action: 'application')
+        "/admin/cfDashboard/service/$serviceName"(controller: 'cloudFoundryDashboard', action: 'service')
+        "/admin/cfDashboard/files/$appName/$instanceIndex?"(controller: 'cloudFoundryDashboard', action: 'files')
+        
+        
         "/"	{
-			controller	= 'home'
-			action		= { 'index' }
+            controller	= 'home'
+            action		= { 'index' }
             view		= { 'index' }
         }
 		"/$controller/$action?/$id?"{
-			constraints {
-				controller(matches:/^((?!(api|mobile|web)).*)$/)
-		  	}
-		}
+            constraints {
+                controller(matches:/^((?!(api|mobile|web)).*)$/)
+            }
+        }
 		
-		/* 
-		 * System Pages without controller 
-		 */
+        /* 
+         * System Pages without controller 
+         */
 		"500"	(view:'/error')
-	}
+    }
 }
